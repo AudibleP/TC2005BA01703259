cfef0a245f64f3ad661cacfd82c28982e5a1db3d
Ricardo Nuñez <ricardonunezalanis@live.com.mx>
2022-02-22 16:56:18 -0600

    Lab 4 completo

--- Lab4/Labo4.js
+++ Lab4/Labo4.js
@@ -1,5 +1,4 @@
 
-//Problema 1
 let boton_prob1 = document.getElementById("problema1");
 function prob1(){
     document.write("Recarga la pagina para regresar");
@@ -12,7 +11,6 @@ function prob1(){
     document.write("</table>");
 }
 
-//Problema 2
 let boton_prob2 = document.getElementById("problema2");
 function prob2(){
     let num1 = Math.floor(Math.random() * 20);
@@ -29,7 +27,6 @@ function prob2(){
     document.getElementById("res_prob2").innerHTML = res_prob;
 }
 
-//Problema 3
 let boton_prob3 = document.getElementById("problema3");
 var x = [5,0,-65,1,2,3,4,5,12,33,22,0,0,0,-9,-99,-54,1,2,3,0,0,0,0,3,46,89,-8,-87];
 function prob3(arreglo){
@@ -48,14 +45,13 @@ function prob3(arreglo){
             positivos++;
         }
     }
-    let res_prob = `Arreglo: ${x}<br> Numeros negativos: ${negativos}<br> Cantidad de 0s: ${ceros}<br> Numeros positivos: ${positivos}<br>`;
+    let res_prob = `Arreglo: ${x}<br> Numeros -s: ${negativos}<br> 0s: ${ceros}<br> Numeros +s: ${positivos}<br>`;
     document.getElementById("res_prob3").innerHTML = res_prob;
 }
 
-//Problema 4
 let boton_prob4 = document.getElementById("problema4");
-let y = [[66,65,36,89,80,43],[77,68,45,56,56,96],[79,27,75,56,8,13],[36,93,72,59,70,48],[92,18,90,38,80,15],[96,28,18,73,87,61]];
-let matriz = "[66,65,36,89,80,43]<br>[77,68,45,56,56,96]<br>[79,27,75,56,8,13]<br>[36,93,72,59,70,48]<br>[92,18,90,38,80,15]<br>[96,28,18,73,87,61]<br>"
+let y = [[1,2,3,4,5,6],[7,6,4,5,5,9],[19,27,35,46,58,63],[6,3,2,9,10,8],[192,218,390,338,280,115],[36,58,28,53,87,11]];
+let matriz = "[1,2,3,4,5,6],[7,6,4,5,5,9],[19,27,35,46,58,63],[6,3,2,9,10,8],[192,218,390,338,280,115],[36,58,28,53,87,11]<br>"
 function prob4(matrix){
     let promedios = 0;
     let promedios_array = new Array; 
@@ -69,30 +65,218 @@ function prob4(matrix){
         promedios = contador/matrix.length;
         promedios_array.push((" " + promedios.toFixed(2))); 
     }
-    let res_prob = `La matriz original: <br> ${matriz} <br> Los promedios de cada renglon son: ${promedios_array}`;
+    let res_prob = `Matriz original: <br> ${matriz} <br> Los promedios son: ${promedios_array}`;
     document.getElementById("res_prob4").innerHTML = res_prob;
 }
 
-//Problema 5
-let boton_prob5 = document.getElementById("problema5");
-var num_inv = 0;
-function prob5(z) {
-    var num_inv = prompt("Ingresa un número para conocer su inverso.");
-    z = num_inv;
-    let inverso = z.split('').reverse().join('');
+var numero = Math.floor( Math.random() * 100);
 
-    let res_prob = `El inverso del numero que ingresaste es : ${inverso}`;
-    document.getElementById("res_prob5").innerHTML = res_prob;
+function inverso(numero){
+
+  var invertido = 0
+  var resto = numero
+  do {
+    invertido = invertido * 10 + (resto % 10)
+    resto = Math.floor(resto / 10)
+  } while ( resto > 0 )
+
+  respuesta = "Número = " + numero +"<br></br>" + "Invertido: " + invertido
+  return respuesta
+
+}
+document.getElementById("resultados5").innerHTML = inverso(numero);
+
+
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
 }
 
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
 
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('btn')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
 
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
 
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
+
+const textNodes = [
+  {
+    id: 1,
+    text: 'Despiertas despues de un dia muy pesado y tienes que ir al Tec, antes de irte ves tu mochila con tu laptop.',
+    options: [
+      {
+        text: 'Llevar laptop',
+        setState: { laptop: true },
+        nextText: 2
+      },
+      {
+        text: 'No llevar laptop',
+        nextText: 2
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'Al llegar al tec y dirigirte a tu primera clase (Construcción de software y toma de decisiones) decides:',
+    options: [
+      {
+        text: 'Sacar tu laptop para prestar toda la atencion.',
+        requiredState: (currentState) => currentState.laptop,
+        nextText: 3
+      },
+      {
+        text: 'No sacar la laptop y prestar media atencion',
+        nextText: 4
+      },
+      {
+        text: 'No entrar a la clase...',
+        nextText: 5
+      }
+    ]
+  },
+  {
+    id: 3,
+    text: 'Despues de que terminan las clases de Lalo y Ricardo, se acercan a ti y te otorgan un 100 en la materia.',
+    options: [
+      {
+        text: 'Felicidades, Jugar otra vez?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 4,
+    text: 'Te quedas medio dormido en clase, pero Lalo y Ricardo no se dan cuanta y al final pasa la materia con 70.',
+    options: [
+      {
+        text: 'Felicidades??? Jugar otra vez?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 5,
+    text: 'Ya que no estraste a clase tienes varias opciones.',
+    options: [
+      {
+        text: 'Ir a desayunar.',
+        nextText: 6
+      },
+      {
+        text: 'Ir a jugar con tus amigos.',
+        nextText: 7
+      },
+      {
+        text: 'Ir a estaduiar a biblioteca',
+        requiredState: (currentState) => currentState.laptop,
+        setState: { laptop: false, conocimiento: true },
+        nextText: 8
+      },
+      {
+        text: 'Regresar a tu casa',
+        nextText: 9
+      }
+    ]
+  },
+  {
+    id: 6,
+    text: 'Desayunas chilaquiles verdes en Cafe y aunque reprobaste lo hiciste con estomago lleno.',
+    options: [
+      {
+        text: 'Jugar otra vez?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 7,
+    text: 'Reprobaste la materia, pero te divertiste con tus amigos.',
+    options: [
+      {
+        text: 'Jugar otra vez?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 8,
+    text: 'Estudiaste bastante para poder pasar la materia, decides:.',
+    options: [
+      {
+        text: 'Regresar a tu casa.',
+        nextText: 10
+      },
+      {
+        text: 'Intentar pasar la materia.',
+        nextText: 11
+      }
+    ]
+  },
+  {
+    id: 9,
+    text: 'Te sientes descansado, pero reprobaste la materia...',
+    options: [
+      {
+        text: 'Jugar otra vez?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 10,
+    text: 'PORQUE REGRESASTE?! YA ESTABAS PREPARADO!!! MUY MAL...',
+    options: [
+      {
+        text: 'Reprobaste, jugar otra vez?',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 11,
+    text: 'Despues de una leccion de vida de Ricardo y Lalo, logras pasar la materia.',
+    options: [
+      {
+        text: 'Filicidades, jugar otra vez?.',
+        nextText: -1
+      }
+    ]
+  }
+]
+
+startGame()
 
 
 boton_prob1.onclick = prob1;
 boton_prob2.onclick = prob2;
 boton_prob3.onclick = prob3(x);
-boton_prob4.onclick = prob4;
-boton_prob5.onclick = prob5;
-boton_prob6.onclick = prob6;
+boton_prob4.onclick = prob4(y);
